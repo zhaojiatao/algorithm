8a1bc2ddf5b3a57f2edb5edc3a524ce5f86491cc
zhaojiatao <2322795718@qq.com>
2018-07-06 11:02:14 +0800

    增加tree

---
+++ src/main/java/map/HashMapTest.java
@@ -0,0 +1,29 @@
+
+package map;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhaojiatao
+ * @date 2018/6/26
+ */
+public class HashMapTest {
+
+
+
+
+    @Test
+    public void test01(){
+
+        Map<String,Object> map =new HashMap<String,Object>(16);
+        map.put("abc","efg");
+
+    }
+
+
+
+
+}
